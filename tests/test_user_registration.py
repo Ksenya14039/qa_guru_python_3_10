3a45a6d0498a716348770cacdb0d98aafc248dcd
ksenya14039 <ksenya14039@mail.ru>
2023-02-16 21:32:27 +0300

    add allure

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,43 +1,47 @@
+import allure
 from demoqa_tests.model.pages import practice_form
 
 
+@allure.title('Successful fill form')
 def test_filling_form():
-    practice_form.open_page()
+    with allure.step('Open registration form'):
+        practice_form.open_page()
 
     # WHEN
-    practice_form.input_info(name='Ksenia',
+    with allure.step('Fill form'):
+        practice_form.input_info(name='Ksenia',
                              surname='Kapranova',
                              email='ksenya14039@mail.ru',
                              mobile='9973655228',
                              address='Moscow'
                              )
 
-    practice_form.select_gender('Female')
+        practice_form.select_gender('Female')
 
-    practice_form.select_birthday(day='9', month='9', year='1997')
+        practice_form.select_birthday(day='9', month='9', year='1997')
 
-    practice_form.input_subject('English')
+        practice_form.input_subject('English')
 
-    practice_form.select_hobbies('Reading')
+        practice_form.select_hobbies('Reading')
 
-    practice_form.upload_picture('resources/wepk.jpeg')
+        practice_form.upload_picture('resources/wepk.jpeg')
 
-    practice_form.select_state('NCR')
-    practice_form.select_city('Delhi')
+        practice_form.select_state('NCR')
+        practice_form.select_city('Delhi')
 
-    practice_form.submit()
+        practice_form.submit()
 
     # THEN
-
-    practice_form.assert_of_registered_user(
-        'Ksenia Kapranova',
-        'ksenya14039@mail.ru',
-        'Female',
-        '9973655228',
-        '9 October,1997',
-        'English',
-        'Reading',
-        'wepk.jpeg',
-        'Moscow',
-        'NCR Delhi'
-    )
+    with allure.step('Check form results'):
+        practice_form.assert_of_registered_user(
+            'Ksenia Kapranova',
+            'ksenya14039@mail.ru',
+            'Female',
+            '9973655228',
+            '9 October,1997',
+            'English',
+            'Reading',
+            'wepk.jpeg',
+            'Moscow',
+            'NCR Delhi'
+        )
